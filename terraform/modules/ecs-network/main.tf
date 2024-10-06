238de878392449d32b39099477db0f59a2238633
Samuel McManus <samuelmcmanus819@gmail.com>
2024-10-06 01:41:17 -0500

    Perform inline ignores

--- terraform/modules/ecs-network/main.tf
+++ terraform/modules/ecs-network/main.tf
@@ -97,7 +97,7 @@ resource "aws_security_group" "web_server_sg" {
   }
 }
 
-# 
+#trivy:ignore:AVD-AWS-0107
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.web_server_sg.id
   cidr_ipv4         = "0.0.0.0/0"
@@ -106,6 +106,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   to_port           = 8080
 }
 
+#trivy:ignore:AVD-AWS-0107
 resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv6" {
   security_group_id = aws_security_group.web_server_sg.id
   cidr_ipv6         = "::/0"
@@ -114,6 +115,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv6" {
   to_port           = 8080
 }
 
+#trivy:ignore:AVD-AWS-0104
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.web_server_sg.id
   cidr_ipv4         = "0.0.0.0/0"
