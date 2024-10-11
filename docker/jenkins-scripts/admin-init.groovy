05c8c0dfa93552a0268d6b36412a56549c51e68b
Samuel McManus <samuelmcmanus819@gmail.com>
2024-10-11 14:14:37 -0500

    Update admin-init.groovy

--- docker/jenkins-scripts/admin-init.groovy
+++ docker/jenkins-scripts/admin-init.groovy
@@ -1,33 +1,33 @@
 import jenkins.model.*
 import hudson.security.*
+import jenkins.security.s2m.AdminWhitelistRule
 
+// Get Jenkins instance
 def instance = Jenkins.getInstance()
 
-// Get the admin user credentials from environment variables
+// Setup security realm to allow Jenkins to manage user authentication
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 def adminUsername = System.getenv("JENKINS_ADMIN_ID")
 def adminPassword = System.getenv("JENKINS_ADMIN_PASSWORD")
 
-println "--> Setting up Security Realm and Authorization Strategy"
-
-// Create admin user if it doesn't exist
-def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+// Add the admin user
 hudsonRealm.createAccount(adminUsername, adminPassword)
 instance.setSecurityRealm(hudsonRealm)
 
-// Set up Global Matrix Authorization Strategy
+// Setup authorization strategy to use Role-Based Access Control (RBAC)
 def strategy = new GlobalMatrixAuthorizationStrategy()
 
-// Grant full admin privileges to the admin user
+// Grant full admin permissions to the admin user
 strategy.add(Jenkins.ADMINISTER, adminUsername)
 
-// Remove ALL permissions for anonymous users to force login
-// This removes any default permissions like Jenkins.READ
-println "--> Revoking all permissions for anonymous users"
-strategy.add(Jenkins.READ, "authenticated") // Only authenticated users can read
+// Disallow anonymous users from accessing the dashboard (i.e., authenticated users only)
+strategy.add(Jenkins.READ, 'authenticated')
 
-// Apply the new strategy to Jenkins
+// Set the authorization strategy
 instance.setAuthorizationStrategy(strategy)
-instance.save()
 
-println "--> Admin user ${adminUsername} has been granted full access."
-println "--> Anonymous users will
+// Disable the agent-to-controller security whitelist
+Jenkins.instance.injector.getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
+
+// Save the Jenkins configuration
+instance.save()
