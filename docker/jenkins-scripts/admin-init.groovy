ca23232aa22f84ef49b988ab246d971eac7c0d57
Samuel McManus <samuelmcmanus819@gmail.com>
2024-10-11 14:40:20 -0500

    Update admin-init.groovy

--- docker/jenkins-scripts/admin-init.groovy
+++ docker/jenkins-scripts/admin-init.groovy
@@ -1,33 +1,21 @@
 import jenkins.model.*
 import hudson.security.*
-import jenkins.security.s2m.AdminWhitelistRule
 
-// Get Jenkins instance
 def instance = Jenkins.getInstance()
 
-// Setup security realm to allow Jenkins to manage user authentication
-def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 def adminUsername = System.getenv("JENKINS_ADMIN_ID")
 def adminPassword = System.getenv("JENKINS_ADMIN_PASSWORD")
 
-// Add the admin user
-hudsonRealm.createAccount(adminUsername, adminPassword)
-instance.setSecurityRealm(hudsonRealm)
+if (adminUsername && adminPassword) {
+    def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+    hudsonRealm.createAccount(adminUsername, adminPassword)
+    instance.setSecurityRealm(hudsonRealm)
 
-// Setup authorization strategy to use Role-Based Access Control (RBAC)
-def strategy = new GlobalMatrixAuthorizationStrategy()
+    def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+    instance.setAuthorizationStrategy(strategy)
+    instance.save()
 
-// Grant full admin permissions to the admin user
-strategy.add(Jenkins.ADMINISTER, adminUsername)
-
-// Disallow anonymous users from accessing the dashboard (i.e., authenticated users only)
-strategy.add(Jenkins.READ, 'authenticated')
-
-// Set the authorization strategy
-instance.setAuthorizationStrategy(strategy)
-
-// Disable the agent-to-controller security whitelist
-Jenkins.instance.injector.getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
-
-// Save the Jenkins configuration
-instance.save()
+    println "--> Admin user ${adminUsername} created."
+} else {
+    println "--> Admin credentials not provided, skipping user creation."
+}
